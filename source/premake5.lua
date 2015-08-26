616852dbe96d468e9d5d89505838b5abf96f1c06
kornislav <kornislav.litsgard@gmail.com>
2015-08-26 11:19:39 +0200

    Add premake utils for creating project

---
+++ source/premake5.lua
@@ -0,0 +1,77 @@
+PRODUCT_NAME = 'Engine'
+
+solution(PRODUCT_NAME)
+	platforms {
+		"Win32"
+	}
+	configurations {
+		"Debug",
+		"Release",
+		"Final"
+	}
+
+project(PRODUCT_NAME)
+	kind "WindowedApp"
+	language "C++"
+	files {
+		"code/**.cpp",
+		"code/**.h",
+	}
+
+	targetdir "$(ProjectDir)../bin/"
+	targetname ("$(ProjectName)_$(PlatformName)_$(Configuration)_" .. _ACTION)
+	debugdir "$(ProjectDir)../bin/"
+	objdir ("$(ProjectDir)../build/" .. _ACTION)
+
+	includedirs {
+		"$(SolutionDir)/code"
+	}
+
+	-- Global per-configuration settings.
+	configuration { "Debug" }
+		defines {
+			"_DEBUG",
+			"DEBUG",
+		}
+		flags {
+			"Symbols",
+		}
+
+	configuration { "Release" }
+		defines {
+			"RELEASE",
+			"NDEBUG",
+		}
+		flags {
+			"Symbols",
+			"OptimizeSpeed",
+		}
+
+	configuration { "Final" }
+		defines {
+			"FINAL",
+			"NDEBUG",
+		}
+		flags {
+			"OptimizeSpeed",
+		}
+
+	-- Win32
+	configuration { "Win32" }
+		defines {
+			"WIN32",
+			"_WINDOWS",
+		}
+		links {
+
+		}
+
+	configuration { "Win32 or Oculus", "not Debug" }
+		buildoptions {
+			-- Whole program optimization
+			"/GL"
+		}
+		linkoptions {
+			-- Link-time code generation
+			"/LTCG"
+		}
