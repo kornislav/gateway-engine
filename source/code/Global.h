753878ea60df0056fd2e47a4c0324cf4b291a60d
kornislav <kornislav.litsgard@gmail.com>
2015-08-27 14:33:53 +0200

    Add a useful function

--- source/code/Global.h
+++ source/code/Global.h
@@ -1,7 +1,17 @@
 #pragma once
 
+// includes
 #ifdef WIN32
 #include <windows.h>
 #endif
 
+// typedefs
 typedef unsigned int uint;
+
+// macros
+
+// functions
+static void memclr(void* data, size_t size)
+{
+	memset(data, 0, size);
+}
