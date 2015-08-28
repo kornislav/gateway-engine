95d37dc8f5f85b1127e108dffbe39ec9a3545889
Markus Pursche <pursche01@gmail.com>
2015-08-28 11:02:57 +0200

    Add filetypes to include
    Added .c, .hpp, .inl

--- source/premake5.lua
+++ source/premake5.lua
@@ -14,8 +14,11 @@ project(PRODUCT_NAME)
 	kind "WindowedApp"
 	language "C++"
 	files {
+		"code/**.c",
 		"code/**.cpp",
 		"code/**.h",
+		"code/**.hpp",
+		"code/**.inl",
 	}
 
 	targetdir "$(ProjectDir)../bin/"
